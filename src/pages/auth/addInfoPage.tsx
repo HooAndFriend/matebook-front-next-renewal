386cf945331e6b5498d81a9d54fb9ec962026980
pinomaker-hoo <inhoo23@naver.com>
2023-02-10 01:59:05 +0900

    Fix : 유저 정보 입력 View 구현 완료

--- src/pages/auth/addInfoPage.tsx
+++ src/pages/auth/addInfoPage.tsx
@@ -1,3 +1,112 @@
-function AddinfoPage() {}
+// ** Next Imports
+import Link from "next/link"
+
+// ** Assets Imports
+import Color from "@/assets/color"
+
+// ** Mui Imports
+import { Button, Card, Grid, TextField, Typography } from "@mui/material"
+
+function AddinfoPage() {
+  return (
+    <Grid container spacing={6} sx={{ px: 60, py: 10 }}>
+      <Grid item xs={12}>
+        <Typography variant="h4" color="white">
+          matebook
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          label="메이트 닉네임"
+          fullWidth
+          sx={{ backgroundColor: Color.purple, borderRadius: 1.5 }}
+          name="email"
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          label="최근의 읽은 책"
+          fullWidth
+          sx={{ backgroundColor: Color.purple, borderRadius: 1.5 }}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h6" color="white">
+          관심 있는 책
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Grid container spacing={6}>
+          <Grid item xs={4}>
+            <Card sx={{ minHeight: 250 }}>
+              <Typography>책</Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            {" "}
+            <Card sx={{ minHeight: 250 }}>
+              <Typography>책</Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            {" "}
+            <Card sx={{ minHeight: 250 }}>
+              <Typography>책</Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            {" "}
+            <Card sx={{ minHeight: 250 }}>
+              <Typography>책</Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            {" "}
+            <Card sx={{ minHeight: 250 }}>
+              <Typography>책</Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            {" "}
+            <Card sx={{ minHeight: 250 }}>
+              <Typography>책</Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            {" "}
+            <Card sx={{ minHeight: 250 }}>
+              <Typography>책</Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            {" "}
+            <Card sx={{ minHeight: 250 }}>
+              <Typography>책</Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            {" "}
+            <Card sx={{ minHeight: 250 }}>
+              <Typography>책</Typography>
+            </Card>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={4} />
+      <Grid item xs={4}>
+        <Link href="/home">
+          <Button
+            fullWidth
+            variant="contained"
+            sx={{ backgroundColor: Color.purple, borderRadius: 1.5 }}
+          >
+            시작하기
+          </Button>
+        </Link>
+      </Grid>
+      <Grid item xs={4} />
+    </Grid>
+  )
+}
 
 export default AddinfoPage
