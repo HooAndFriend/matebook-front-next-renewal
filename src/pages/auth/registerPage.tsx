11bfe9bdeeed1fb4609a4ef377254a932ac12e29
pinomaker-hoo <inhoo23@naver.com>
2023-02-10 01:58:52 +0900

    Fix : 회원가입 View 구현 완료

--- src/pages/auth/registerPage.tsx
+++ src/pages/auth/registerPage.tsx
@@ -1,56 +1,147 @@
+// ** Next Imports
+import Link from "next/link"
+
+// ** React Imports
+import { useState } from "react"
+
+// ** Assets Imports
 import Color from "@/assets/color"
-import { Button, Grid, TextField, Typography } from "@mui/material"
+
+// ** Mui Imports
+import {
+  Button,
+  Grid,
+  TextField,
+  Typography,
+  InputLabel,
+  MenuItem,
+  Select,
+  FormControl,
+  SelectChangeEvent,
+} from "@mui/material"
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker"
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
+
+// ** Date Imports
+import dayjs, { Dayjs } from "dayjs"
 
 function RegisterPage() {
+  const [value, setValue] = useState<Dayjs | null>(dayjs("2014-08-18T21:11:54"))
+  const [age, setAge] = useState("")
+
+  const handleChangeAge = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string)
+  }
+
+  const handleChangeDate = (newValue: Dayjs | null) => {
+    setValue(newValue)
+  }
   return (
-    <Grid container spacing={6} sx={{ px: 120, py: 40 }}>
+    <Grid container spacing={6} sx={{ px: 60, py: 10 }}>
       <Grid item xs={12}>
-        <Typography>matebook</Typography>
+        <Typography variant="h4" color="white">
+          matebook
+        </Typography>
       </Grid>
       <Grid item xs={6}>
-        <TextField fullWidth />
+        <TextField
+          label="이름"
+          fullWidth
+          sx={{
+            backgroundColor: Color.purple,
+            borderRadius: 1.5,
+          }}
+          name="name"
+        />
       </Grid>
-      <Grid item xs={3}>
-        <TextField fullWidth />
+      <Grid item xs={6}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={age}
+            label="Age"
+            onChange={handleChangeAge}
+            sx={{
+              backgroundColor: Color.purple,
+              borderRadius: 1.5,
+            }}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
       </Grid>
-      <Grid item xs={3}>
-        <TextField fullWidth />
-      </Grid>
-      <Grid item xs={4}>
-        <TextField fullWidth />
-      </Grid>
-      <Grid item xs={4}>
-        <TextField fullWidth />
-      </Grid>
-      <Grid item xs={4}>
-        <TextField fullWidth />
+
+      <Grid item xs={12}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DesktopDatePicker
+            label="생일"
+            inputFormat="MM/DD/YYYY"
+            value={value}
+            onChange={handleChangeDate}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                fullWidth
+                sx={{ backgroundColor: Color.purple, borderRadius: 1.5 }}
+              />
+            )}
+          />
+        </LocalizationProvider>
       </Grid>
       <Grid item xs={8}>
-        <TextField fullWidth />
+        <TextField
+          label="Email"
+          fullWidth
+          sx={{ backgroundColor: Color.purple, borderRadius: 1.5 }}
+          name="email"
+        />
       </Grid>
       <Grid item xs={4}>
-        <Button variant="contained" fullWidth>
+        <Button
+          variant="contained"
+          fullWidth
+          sx={{ backgroundColor: Color.purple, minHeight: 55 }}
+        >
           코드 전송
         </Button>
       </Grid>
       <Grid item xs={12}>
-        <TextField fullWidth />
-      </Grid>
-      <Grid item xs={12}>
-        <TextField fullWidth />
-      </Grid>
-      <Grid item xs={12}>
-        <TextField fullWidth />
-      </Grid>
-
-      <Grid item xs={12}>
-        <Button
-          variant="contained"
+        <TextField
+          label="인증 코드"
           fullWidth
-          sx={{ backgroundColor: Color.kakao }}
-        >
-          카카오계정 로그인
-        </Button>
+          sx={{ backgroundColor: Color.purple, borderRadius: 1.5 }}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          label="password"
+          fullWidth
+          sx={{ backgroundColor: Color.purple, borderRadius: 1.5 }}
+          name="password"
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          label="password check"
+          fullWidth
+          sx={{ backgroundColor: Color.purple, borderRadius: 1.5 }}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Link href="/auth/addInfoPage">
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{ backgroundColor: Color.purple, minHeight: 55 }}
+          >
+            회원가입
+          </Button>
+        </Link>
       </Grid>
     </Grid>
   )
